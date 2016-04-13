10b71dbd14a71b18739f91f3eec8975c74870b6f
Evan Wills <evan.wills@acu.edu.au>
2016-04-14 09:31:10 +1000

    updated php/regex_tester.class.php & php/regex_tester.json.api.php; got API service working.

--- php/regex_tester.class.php
+++ php/regex_tester.class.php
@@ -1,32 +1,400 @@
 <?php
 
-class regex_tester {
-	protected $find = '';
-	protected $replace = '';
-	protected $modifiers = '';
-	protected $id = 0;
+if( !class_exists('micro_time') ) { die('can\'t function without micro_time class'); }
+if( !class_exists('handle_php_errors') ) { die('can\'t function without handle_php_errors class'); }
 
+class regex_tester {
+	protected $error_message = '';
+	protected $find = '';
+	protected $id = 0;
+	protected $ok = true;
+	protected $replace = '';
+	protected $regex = '';
+	protected $matched = false;
+
+	static protected $handle_errors = null;
 	static protected $delim_open = '`';
 	static protected $delim_close = '`';
 
-	public function __construct($find, $modifiers, $replace, $id)
+	protected function __construct( $id , $find , $modifiers , $replace = '' , $errors = false )
 	{
+		$this->id = $id;
+		$this->find = $find;
+		$this->replace = $replace;
+		$this->modifiers = $modifiers;
+		$this->regex = self::$delim_open.$this->find.self::$delim_close.$this->modifiers;
 
+		if( is_string($errors) && $errors !== '' )
+		{
+			$this->error_message = $errors;
+		}
+	}
+
+	public static function get_obj( $regex_pair, $do_replace = false )
+	{
+		if( self::$handle_errors === null )
+		{
+			self::$handle_errors = new handle_php_errors();
+		}
+
+		$error_message = '';
+		if( !is_numeric($regex_pair->id) && !is_string($regex_pair->id) )
+		{
+			throw new Exception('regex_tester::get_obj() expects first parameter to be a number or a string. '.gettype($id).' given');
+		}
+		if( !is_string($regex_pair->find) || $regex_pair->find === '' ) {
+			throw new exception('regex_tester::__construct() expects second param $find to be a non empty string');
+		}
+		if( !is_string($regex_pair->modifiers) )
+		{
+			throw new Exception('regex_tester::__construct() expects third param $modifiers to be a string');
+		}
+		if( preg_match('`[^eimsuxADJSUX]`', $regex_pair->modifiers) )
+		{
+			$error_message = "\$modifiers can containing no more than one of each of the following characters: \"e\", \"i\", \"m\", \"s\", \"u\", \"x\", \"A\", \"D\", \"j\", \"S\", \"U\" and/or \"X\"";
+			$modifiers = preg_replace( '`[^eimsuxADJSUX]+`' , '' , $regex_pair->modifiers );
+		}
+		else
+		{
+			$modifiers = $regex_pair->modifiers;
+		}
+
+		if( !is_string($regex_pair->replace) )
+		{
+			throw new Exception('regex_tester::__construct() expects fourth param $replace to be a string');
+		}
+
+
+		// ==========================================================
+		// START: managing PHP errors
+
+		if($old_track_errors = ini_get('track_errors'))
+		{
+			$old_php_errormsg = isset($php_errormsg)?$php_errormsg:false;
+		}
+		else
+		{
+			ini_set('track_errors' , 1);
+		}
+		unset($php_errormsg);
+
+		$display_errors = ini_get('display_errors');
+		ini_set('display_errors',0);
+
+		$html_errors = ini_get('html_errors');
+		ini_set('html_errors','false');
+
+		// ----------------------------------------------------------
+		// START: doing the do
+
+		preg_match(self::$delim_open.$regex_pair->find.self::$delim_close.$modifiers,'');
+
+
+		if( isset($php_errormsg) )
+		{
+			$error = $php_errormsg;
+			unset( $php_errormsg );
+		}
+		else
+		{
+			debug('No there wasn\'t an error');
+			$error = false;
+		}
+
+		// END: doing the do
+		// ----------------------------------------------------------
+
+		ini_set('display_errors',$display_errors);
+		ini_set('html_errors',$html_errors);
+
+		if($old_track_errors)
+		{
+			$php_errormsg = isset($old_php_errormsg)?$old_php_errormsg:false;
+		}
+		else
+		{
+			ini_set('track_errors' , 0);
+		}
+
+		// END: managing PHP errors
+		// ==========================================================
+
+		if( $error !== false )
+		{
+			return new regex_tester_error( $regex_pair->id, $regex_pair->find, $modifiers, $regex_pair->replace , array($error_message,$error) );
+		}
+
+		if( $do_replace === true )
+		{
+			return new regex_tester_replace( $regex_pair->id, $regex_pair->find, $modifiers, $regex_pair->replace , $error_message );
+		}
+		else
+		{
+			return new regex_tester_match( $regex_pair->id, $regex_pair->find, $modifiers, $regex_pair->replace , $error_message );
+		}
 	}
 
 	public function get_errors()
 	{
-
+		return array( 'regexID' => $this->id , 'message' => $this->error_message );
 	}
 
-	public function match($sample)
+	public function process($sample)
+	{
+	}
+
+	public function has_errors()
+	{
+		if( $this->error_message !== '')
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public function something_matched()
+	{
+		return $this->matched;
+	}
+
+	static public function set_delim($open,$close)
+	{
+		$tmp = array('open','close');
+		for( $a = 0 ; $a < 2 ; $a += 1 )
+		{
+			if( is_string(${$tmp[$a]}) && strlen(${$tmp[$a]}) === 1 && preg_match('`^[^a-z0-9\s]$`', ${$tmp[$a]}) )
+			{
+				$var = 'delim_'.$tmp[$a];
+				self::$$var = $$tmp[$a];
+			}
+			else
+			{
+				throw new Exception($tmp[$a].' delimiter must be a single non-alpha-numeric, non-white-space character');
+			}
+		}
+	}
+}
+
+
+
+// ==================================================================
+
+
+
+class regex_tester_match extends regex_tester
+{
+	public function process($sample)
 	{
 		$micro = micro_time::get_obj();
+		$output = array(
+			 'regexID' => $this->id
+			,'ok' => true
+			,'matched' => false
+			,'matches' => array()
+			,'seconds' => 0
+		);
+		debug($sample, $this->regex, $this->replace);
+		if( preg_match_all( $this->regex , $sample , $matches , PREG_SET_ORDER) )
+		{
+			for( $a = 0 ; $a < count($matches) ; $a += 1 )
+			{
+				$output['matches'][] = array( 'wholeMatch' => array_shift($matches[$a]) , 'subPatterns' => $matches[$a] );
+			}
+
+			$this->matched = true;
+			$output['matched'] = true;
+			$start = microtime();
+			$sample = preg_replace($this->regex,$this->replace,$sample);
+			$output['seconds'] = $micro->mt_subtract($start,microtime());
+		}
+		return array( 'output' => $output, 'sample' => $sample );
+	}
+}
+
+
+
+// ==================================================================
+
+
+
+class regex_tester_replace extends regex_tester
+{
+	public function process($sample)
+	{
+		$sample = preg_replace($this->regex, $this->replace,$sample);
+		return array( 'output' => array() , 'sample' => $sample );
 
 	}
+}
 
-	public function replace($sample)
+
+
+// ==================================================================
+
+
+
+class regex_tester_error extends regex_tester
+{
+	protected $ok = false;
+	protected $error_processed = false;
+	protected $error_parts = array('good' => '', 'problem' => '', 'bad' => '');
+
+	protected function __construct( $id , $find , $modifiers , $replace = '' , $errors = false )
 	{
+		$this->id = $id;
+		$this->find = $find;
+		$this->replace = $replace;
+		$this->regex = self::$delim_open.$this->find.self::$delim_close.$modifiers;
 
+		$this->error_message = array();
+
+		for( $a = 0 ; $a < count($errors) ; $a += 1 )
+		{
+			if( $errors[$a] !== '' )
+			{
+				$this->error_message[] = $errors[$a];
+			}
+		}
+	}
+
+	public function get_errors()
+	{
+		$message = array();
+		$output = array( 'regexID' => $this->id , 'message' => array() );
+		$problem_parts = array();
+		$sep = '';
+
+		$a = ( count($this->error_message) - 1 );
+		$this->error_message[$a] = $this->parse_errors($this->error_message[$a]);
+
+		for( $a = 0 ; $a < count($this->error_message) ; $a += 1 ) {
+			$output['message'][] = $this->error_message[$a];
+		}
+
+		if( $this->error_parts['problem'] !== '' && $this->error_parts['error'] !== '' )
+		{
+			$output['problemParts'] = array(
+				 'good' => $this->error_parts['good']
+				,'problem' => $this->error_parts['problem']
+				,'error' => $this->error_parts['error']
+			);
+		}
+
+		return $output;
+	}
+
+	public function process($sample)
+	{
+		return array( 'output' => array( 'regexID' => $this->id , 'ok' => false ) , 'sample' => $sample );
+	}
+
+	public function has_error()
+	{
+		return true;
+	}
+
+/**
+ * @method errors() returns all error information generated by the
+ * object, including input and preg errors
+ *
+ * @param void
+ * @return array an array of strings with error messages.
+ */
+	private function parse_errors($error)
+	{
+		if( $this->error_processed === false )
+		{
+			if( preg_match('/missing (?:terminating )?([\]\}\)]) .*? offset ([0-9]+)/' , $error , $matches ) )
+			{
+				$bracket = $matches[1];
+				$offset = $matches[2] += 1;
+				preg_match('/^(.{'.$offset.'})(.*)$/is' , $this->regex , $matches );
+				$head = $matches[1];
+				$tail = $matches[2];
+				switch($bracket)
+				{
+					case ']': $bracket_ = '[';
+						break;
+					case '}': $bracket_ = '{';
+						break;
+					case ')': $bracket_ = '(';
+						break;
+					case '[': $bracket_ = ']';
+						break;
+					case '{': $bracket_ = '}';
+						break;
+					case '(': $bracket_ = ')';
+						break;
+				}
+				preg_match( '/^(.*?\\'.$bracket_.')(.*)$/is' , $head , $matches );
+				$this->error_parts['good'] = $matches[1];
+				$this->error_parts['problem'] = $matches[2];
+				$this->error_parts['error'] = $tail;
+			}
+			elseif( preg_match('/Unknown modifier/is' , $error ) )
+			{
+				preg_match('/^./',$this->find,$matches);
+				switch($matches[0])
+				{
+					case '(': $wrap = ')';
+						break;
+					case '[': $wrap = ']';
+						break;
+					case '{': $wrap = '}';
+						break;
+					case '<': $wrap = '>';
+						break;
+					default: $wrap = $matches[0];
+				}
+				$regex = '/^.(.*?)(?<!\\\\)(\\'.$wrap.')(.*)$/s';
+				preg_match($regex,$this->find,$matches);
+				$this->error_parts['good'] = $matches[1];
+				$this->error_parts['problem'] = $matches[2];
+				$this->error_parts['error'] = $matches[3];
+			}
+			elseif( preg_match('/at offset ([0-9]+)/is' , $error , $matches ) )
+			{
+				$offset = ++$matches[1];
+				preg_match('/^(.{'.$offset.'})(.)(.*)$/is' , $this->find , $matches );
+				$this->error_parts['good'] = $matches[1];
+				$this->error_parts['problem'] = $matches[2];
+				$this->error_parts['error'] = $matches[3];
+			}
+			elseif( preg_match('/lookbehind assertion is not fixed length at offset ([0-9]+)/is' , $error , $matches ) )
+			{
+				$offset = ++$matches[1];
+				preg_match('/^(.{'.$offset.'})(.*)$/is' , $this->find , $matches );
+				$head = $matches[1];
+				$tail = $matches[2];
+				preg_match('/^(.*)(\(\?<[=!].*)/is' , $head , $matches );
+				$this->error_parts['good'] = $matches[1];
+				$this->error_parts['problem'] = $matches[2];
+				$this->error_parts['error'] = $tail;
+			}
+			elseif( preg_match('/No ending (?:matching )?delimiter \'([^\']+)\' found/is' , $error , $matches ) )
+			{
+				$delim = $matches[1];
+				preg_match('/^(.)(.*)$/is' , $this->find , $matches);
+				$this->highlight = '<span class="ok">'.$this->h($matches[1]).'</span><span class="error">'.$this->h($matches[2]).'</span>';
+				$this->error_parts['good'] = $matches[1];
+				$this->error_parts['problem'] = '';
+				$this->error_parts['error'] = $matches[2];
+			}
+			elseif( preg_match('/Delimiter must not be alphanumeric or backslash/i', $error , $matches ) )
+			{
+				preg_match('/^(.)(.*)$/is' , $this->find , $matches);
+				$this->error_parts['good'] = $matches[1];
+				$this->error_parts['problem'] = '';
+				$this->error_parts['error'] = $matches[2];
+			}
+			else
+			{
+//				die("PREG encountered an error I couldn't recognise (or at least haven't seen yet): \"$error\"");
+			}
+
+			$this->error_processed = true;
+			return str_replace('preg_match() [function.preg-match]: ','',strip_tags($error));
+		}
+		return $error;
 	}
 }
--- php/regex_tester.json.api.php
+++ php/regex_tester.json.api.php
@@ -1,7 +1,82 @@
 <?php
 
+header('content-type:application/json');
+
+
+// ==================================================================
+// START: debug include
+
+if(!function_exists('debug'))
+{
+	if(isset($_SERVER['HTTP_HOST'])){ $path = $_SERVER['HTTP_HOST']; $pwd = dirname($_SERVER['SCRIPT_FILENAME']).'/'; }
+	else { $path = $_SERVER['USER']; $pwd = $_SERVER['PWD'].'/'; };
+	if( substr_compare( $path , '192.168.' , 0 , 8 ) == 0 ) { $path = 'localhost'; }
+	switch($path)
+	{
+		case '192.168.18.128':	// work laptop (debian)
+		case '192.168.1.128':	// raspberry Pi
+		case 'antechinus':	// work laptop (debian)
+		case 'evan':		// home laptop
+		case 'wombat':	$root = '/var/www/html/';	$inc = $root.'includes/'; $classes = $cls = $root.'classes/'; break; // home laptop
+
+		case 'localhost':	$root = 'c:/wamp/www/';	$inc = $root.'includes/'; $classes = $cls = $root.'classes/'; break; // home laptop
+
+		case 'burrawangcoop.net.au':	// DreamHost
+		case 'adra.net.au':		// DreamHost
+		case 'canc.org.au':		// DreamHost
+		case 'ewills':	$root = '/home/ewills/evan/'; $inc = $root.'includes/'; $classes = $cls = $root.'classes/'; break; // DreamHost
+
+		case 'apps.acu.edu.au':		// ACU
+		case 'testapps.acu.edu.au':	// ACU
+		case 'dev1.acu.edu.au':		// ACU
+		case 'blogs.acu.edu.au':	// ACU
+		case 'studentblogs.acu.edu.au':	// ACU
+		case 'dev-blogs.acu.edu.au':	// ACU
+		case 'evanw':	$root = '/home/evanw/';	$inc = $root.'includes/'; $classes = $cls = $root.'classes/'; break; // ACU
+
+		case 'webapps.acu.edu.au':	   // ACU
+		case 'panvpuwebapps01.acu.edu.au': // ACU
+		case 'test-webapps.acu.edu.au':	   // ACU
+		case 'panvtuwebapps01.acu.edu.au': // ACU
+		case 'dev-webapps.acu.edu.au':	   // ACU
+		case 'panvduwebapps01.acu.edu.au': // ACU
+		case 'evwills':
+			if( isset($_SERVER['HOSTNAME']) && $_SERVER['HOSTNAME'] === 'panvtuwebapps01.acu.edu.au' ) {
+				$root = '/home/evwills/'; $inc = $root.'includes/'; $classes = $cls = $root.'classes/'; // ACU
+			} else {
+				$root = '/var/www/html/mini-apps/'; $inc = $root.'includes_ev/'; $classes = $cls = $root.'classes_ev/'; // ACU
+			}
+			break;
+	};
+
+	set_include_path( get_include_path().PATH_SEPARATOR.$inc.PATH_SEPARATOR.$cls.PATH_SEPARATOR.$pwd);
+	if(file_exists($inc.'debug.inc.php'))
+	{
+		if(!file_exists($pwd.'debug.info') && is_writable($pwd) && file_exists($inc.'template.debug.info'))
+		{ copy( $inc.'template.debug.info' , $pwd.'debug.info' ); };
+		require_once($inc.'debug.inc.php');
+	}
+	else { function debug(){}; };
+
+	class emergency_log { public function write( $msg , $level = 0 , $die = false ){ echo $msg; if( $die === true ) { exit; } } }
+};
+
+// END: debug include
+// ==================================================================
+
+
 require_once('micro_time.class.php');
 require_once('regex_tester.class.php');
+
+if( isset($_REQUEST['jquery']) && isset($_REQUEST['callback'] ) )
+{
+	$open = $_REQUEST['callback'].'(';
+	$close = ')';
+}
+else
+{
+	$open = $close = '';
+}
 
 if( !isset($_POST['json']) || !is_string($_POST['json']) || '' === trim($_POST['json']) )
 {
@@ -10,16 +85,64 @@ if( !isset($_POST['json']) || !is_string($_POST['json']) || '' === trim($_POST['
 }
 
 $json = json_decode($_POST['json']);
+//debug($json);
+
+$output = array(
+	'matched' => false,
+	'message' => regex_tester::set_delim($json->delimOpen, $json->delimClose),
+	'regexErrors' => array(),
+	'samples' => array(),
+	'success' => true
+);
+
 $regex_pairs = array();
-
-$output_message = regex_tester::set_delim($json->delimOpen, $json->delimClose);
-
+$has_errors = false;
 for( $a = 0 ; $a < count($json->regexPairs) ; $a += 1 )
 {
-	$regex_pairs[] = new regex_tester(
-		 $json->regexPairs[$a]->find
-		,$json->regexPairs[$a]->modifiers
-		,$json->regexPairs[$a]->replace
-		,$json->regexPairs[$a]->id
-	);
+	$tmp = regex_tester::get_obj($json->regexPairs[$a], $json->doReplace);
+
+	$regex_pairs[] = $tmp;
+	if( $tmp->has_errors() )
+	{
+		$output['regexErrors'][] = $tmp->get_errors();
+		$has_errors = true;
+	}
 }
+
+$matched = false;
+for( $a = 0 ; $a < count($json->sample) ; $a += 1 )
+{
+	$tmp = array( 'sampleID' => $a, 'sampleMatches' => array() );
+
+	for( $b = 0 ; $b < count($regex_pairs) ; $b += 1 )
+	{
+		$tmp_inner = $regex_pairs[$a]->process($json->sample[$a]);
+		$tmp['sampleMatches'][] = $tmp_inner['output'];
+		$json->sample[$a] = $tmp_inner['sample'];
+		if( $matched === false )
+		{
+			$matched = $regex_pairs[$a]->something_matched();
+		}
+	}
+	if( $json->doReplace === true )
+	{
+		$tmp['sampleMatches'] = array($json->sample[$a]);
+	}
+	$output['samples'][] = $tmp;
+}
+
+
+if( $matched === true )
+{
+	$output['matched'] = 'true';
+}
+else
+{
+	$output['samples'] = array();
+	$output['matched'] = 'false';
+}
+
+
+
+
+echo $open.json_encode($output).$close;
